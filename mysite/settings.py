f7f60c8719bd64b5b87b638833282565850c297b
infinity1207 <infinity1207@163.com>
2019-01-18 12:44:40 +0000

    modify mysql setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,21 +14,14 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rents',
+        'USER': 'root',
+        'PASSWORD': '811225',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'test',
-#         'USER': 'root',
-#         'PASSWORD': '811225',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3307',
-#     }
-# }
 
 # bae mysql
 # DATABASES = {
